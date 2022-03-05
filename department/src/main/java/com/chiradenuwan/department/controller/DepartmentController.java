09a3c57874224c6e7e67b478bc78c4dd0db9d8aa
chiradenuwan <chiranjayadenuwan@gmail.com>
2022-03-05 21:42:25 +0530

    add logger for two rest created

--- department/src/main/java/com/chiradenuwan/department/controller/DepartmentController.java
+++ department/src/main/java/com/chiradenuwan/department/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package com.chiradenuwan.department.controller;
 
 import com.chiradenuwan.department.dto.DepartmentDto;
 import com.chiradenuwan.department.service.custom.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
 
     @Autowired
@@ -17,12 +19,14 @@ public class DepartmentController {
 
     @PostMapping("/")
     private ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
+        log.info("Department Save -> Call");
         DepartmentDto departmentDto1 = departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(departmentDto1, HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/{id}")
     private ResponseEntity<DepartmentDto> findDepartment(@PathVariable("id") int departmentId) {
+        log.info("Get Department by Id -> Call");
         DepartmentDto departmentDto1 = departmentService.findDepartment(departmentId);
         return new ResponseEntity<>(departmentDto1, HttpStatus.ACCEPTED);
     }
